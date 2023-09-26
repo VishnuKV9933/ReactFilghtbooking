42d30b692ac1a53429649cb4421f75a117e0e6b5
vishnukv <vishnukv9933@gmail.com>
2023-09-26 12:07:00 +0530

    passengers count added

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import './Service.css'
-import {MdFlight,MdMore,MdReadMore} from 'react-icons/md'
-import {HiStatusOnline} from 'react-icons/hi'
-import Search from '../Search/Search';
+import React from "react";
+import "./Service.css";
+import { MdFlight, MdMore, MdReadMore } from "react-icons/md";
+import { HiStatusOnline } from "react-icons/hi";
+import Search from "../Search/Search";
 
 function Service() {
   return (
     <>
-      <div className='options-container flex  flex-wrap w-full h-fit  px-[20px] lg:px-[150px] '>
-      <div className='w-1/2 md:w-1/4  h-[50px] border flex justify-center items-center font-semibold gap-2'>
-        <MdFlight/>
-        Search Flights
-      </div>
-        <div className='w-1/2 md:w-1/4  h-[50px] border flex justify-center items-center font-semibold gap-2'>
-         <MdMore/>   Manage 
+      <div className="options-container flex  flex-wrap w-full h-fit  px-[20px] lg:px-[150px] ">
+        <div className="w-1/2 md:w-1/4  h-[50px] border flex justify-center items-center font-semibold gap-2">
+          <MdFlight />
+          Search Flights
         </div>
-        <div className='w-1/2 md:w-1/4  h-[50px] border flex justify-center items-center font-semibold gap-2'>
-          <MdReadMore/>  Whats on flight
+        <div className="w-1/2 md:w-1/4  h-[50px] border flex justify-center items-center font-semibold gap-2">
+          <MdMore /> Manage
         </div>
-        <div className='w-1/2 md:w-1/4  h-[50px] border flex justify-center items-center font-semibold gap-2'>
-         <HiStatusOnline/>   Flight Status
+        <div className="w-1/2 md:w-1/4  h-[50px] border flex justify-center items-center font-semibold gap-2">
+          <MdReadMore /> Whats on flight
+        </div>
+        <div className="w-1/2 md:w-1/4  h-[50px] border flex justify-center items-center font-semibold gap-2">
+          <HiStatusOnline /> Flight Status
         </div>
       </div>
 
-        <Search/>
-
+      <Search />
     </>
   );
 }
